fc1e79566c4d61bc72674039783394a2419d0dc0
Nikita Podreshetnikov <programmingakk@gmail.com>
2026-05-19 11:40:26 +0700

    add get_date and get_time

--- main.py
+++ main.py
@@ -11,16 +11,32 @@ bot = telebot.TeleBot(TOKEN)
 reminders = []
 
 
+def get_time(message, reminder_date, reminder_text):
+    reminder_time = message.text
+    reminder = {
+        'text': reminder_text,
+        'date': reminder_date,
+        'time': reminder_time,
+    }
+    reminders.append(reminder)
+
+
+def get_date(message, reminder_text):
+    reminder_date = message.text
+    bot.send_message(message.from_user.id, "Введи время в формате 'ЧЧ:ММ'")
+    bot.register_next_step_handler(message, get_time, reminder_date, reminder_text)
+
+
 def get_reminder(message):
-    reminder_text = message
+    reminder_text = message.text
     bot.send_message(message.from_user.id, "Введи дату напоминания в формате"
                                            " 'ДД.ММ.ГГ'")
-
+    bot.register_next_step_handler(message, get_date, reminder_text)
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     """Функция, принимающая сообщения пользователя."""
-    if message.text == 'Привет' or message.text == '/start':
+    if message.text in ['Привет', '/start']:
         bot.send_message(message.from_user.id, "Привет, это бот-напоминалка.\n"
                                                "Введи 'Напомни', чтобы"
                                                "ничего не забыть!\nВведи /help"
